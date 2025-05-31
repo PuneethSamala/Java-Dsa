5b8f4d5154e5cdd2eda6fd7c17947ca269f98dbc
PuneethSamala <puneethreddy826@gmail.com>
2025-05-31 10:13:20 +0530

    Add files via upload

---
+++ InterfaceExample.java
@@ -0,0 +1,18 @@
+package abc;
+interface printable{
+	void print();
+}
+class Message implements printable{
+	public void print() {
+		System.out.println("Hello from interface");
+	}
+}
+public class InterfaceExample {
+
+	public static void main(String[] args) {
+		Message msg = new Message();
+		msg.print();
+
+	}
+
+}
